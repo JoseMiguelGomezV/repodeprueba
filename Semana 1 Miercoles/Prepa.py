5276a0c25dcabdb55e09225183b97289a1a459e3
JoseMiguelGomezV <113480782+JoseMiguelGomezV@users.noreply.github.com>
2022-09-16 09:37:22 -0400

    Delete Semana 1 Miercoles directory

--- Semana 1 Miercoles/Prepa.py
+++
@@ -1,4 +0,0 @@
-numero_one = input('Coloca el primer numero:')
-numero_two = input('Coloca numero dos:')
-resta = int(numero_one) - int(numero_two)
-print(resta)
--- Semana 1 Miercoles/ejercicio 2.py
+++
@@ -1,4 +0,0 @@
-Hours= int (input ('How many hours did you work ?:'))
-Price= float (input ('How much did you earn p/hour ?:'))
-print(f'Today you have to recive {Hours*Price}')
-
